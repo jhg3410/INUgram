380043366a826c618000e5affd1190cd04600cb7
jhg3410 <jhg34100@naver.com>
2024-04-29 09:59:12 +0900

    VideoPlayerScreen 구현

---
+++ lib/videoplayer/src/main/java/jik/inu/lib/videoplayer/VideoPlayerScreen.kt
@@ -0,0 +1,46 @@
+package jik.inu.lib.videoplayer
+
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
+
+@Composable
+fun VideoPlayerScreen(
+    player: ExoPlayer,
+    modifier: Modifier = Modifier,
+    onScreenClick: () -> Unit
+) {
+
+    val context = LocalContext.current
+
+    Box(
+        modifier = modifier.clickable(
+            indication = null,
+            interactionSource = remember { MutableInteractionSource() },
+            onClick = { onScreenClick() }
+        )
+    ) {
+        AndroidView(
+            factory = {
+                PlayerView(context).apply {
+                    this.useController = false
+                    this.player = player
+                    this.layoutParams = FrameLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                    )
+                }
+            }
+        )
+    }
+}
+
