c9a5cbd4e032dc14734c52ba92022d8bcf52788d
jhg3410 <jhg34100@naver.com>
2024-05-07 01:32:55 +0900

    MyPage 화면 내 정보(이메일, 프로필) 구현

--- feature/mypage/src/main/java/jik/inu/feature/mypage/MyPageScreen.kt
+++ feature/mypage/src/main/java/jik/inu/feature/mypage/MyPageScreen.kt
@@ -1,23 +1,62 @@
 package jik.inu.feature.mypage
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun MyPageScreen(
     modifier: Modifier = Modifier
 ) {
 
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        Spacer(modifier = Modifier.height(20.dp))
+        MyInfo(profileColor = Color(0xFF3780ED), email = "jhg3410")
+    }
+}
+
+@Composable
+private fun MyInfo(profileColor: Color, email: String) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .size(60.dp)
+                .clip(CircleShape)
+                .background(profileColor),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = email.first().toString(),
+                color = Color.White,
+                style = MaterialTheme.typography.titleLarge,
+            )
+        }
+        Spacer(modifier = Modifier.height(12.dp))
         Text(
-            text = "MyPageScreen"
+            text = email,
+            style = MaterialTheme.typography.bodySmall,
         )
     }
 }
