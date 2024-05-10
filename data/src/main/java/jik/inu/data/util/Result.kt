4c7a84af25de2f122b720c851aad0c3cc7d3c180
jhg3410 <jhg34100@naver.com>
2024-05-10 18:08:39 +0900

    jikCatching: http 통신 실패 시 에러 메시지를 받아오도록 변경

--- data/src/main/java/jik/inu/data/util/Result.kt
+++ data/src/main/java/jik/inu/data/util/Result.kt
@@ -1,5 +1,10 @@
 package jik.inu.data.util
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.JsonClass
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.adapter
 import retrofit2.HttpException
 
 internal inline fun <T, R> T.jikCatching(block: T.() -> R): Result<R> {
@@ -8,10 +13,31 @@ internal inline fun <T, R> T.jikCatching(block: T.() -> R): Result<R> {
     } catch (e: Throwable) {
         val exception =
             if (e is HttpException) {
-                Throwable(message = e.response()?.errorBody()?.string())
+                val errorBody = e.response()?.errorBody()?.string()
+                if (errorBody == null) {
+                    return Result.failure(Throwable(message = DEFAULT_ERROR_MESSAGE))
+                } else {
+                    val errorResponse = fromJsonToErrorResponse(errorBody)
+                    Throwable(message = errorResponse.message)
+                }
             } else {
                 e
             }
         Result.failure(exception)
     }
+}
+
+
+private const val DEFAULT_ERROR_MESSAGE = "INUgram 에서 문제가 발생했어요"
+
+@JsonClass(generateAdapter = true)
+data class ErrorResponse(
+    @Json(name = "errorMsg") val message: String
+)
+
+@OptIn(ExperimentalStdlibApi::class)
+fun fromJsonToErrorResponse(json: String): ErrorResponse {
+    val moshi: Moshi = Moshi.Builder().build()
+    val jsonAdapter: JsonAdapter<ErrorResponse> = moshi.adapter<ErrorResponse>()
+    return jsonAdapter.fromJson(json) ?: ErrorResponse(DEFAULT_ERROR_MESSAGE)
 }
